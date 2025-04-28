552dd8fb4ce9c65ed19c58cbd284bb9c5213aac9
Macarena <m.ardevol@alumno.um.edu.ar>
2025-04-28 11:33:12 -0300

    Etapa 2: Agregado comportamiento de pensamiento a Persona

--- src/persona.py
+++ src/persona.py
@@ -3,3 +3,13 @@ class Persona:
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni
+        self.pensamientos = 0
+        self.ultima_idea = "<no pensó en nada>"
+
+    def __repr__(self):
+        return f"Persona: DNI: {self.dni} Nombre: {self.nombre} Apellido: {self.apellido} Ultima Idea: {self.ultima_idea}"
+
+    def pensar(self, idea):
+        """Incrementa el contador de pensamientos y actualiza la última idea."""
+        self.pensamientos += 1
+        self.ultima_idea = idea
--- tests/test_persona.py
+++ tests/test_persona.py
@@ -8,5 +8,20 @@ class TestPersona(unittest.TestCase):
         self.assertEqual(persona.apellido, "Pérez")
         self.assertEqual(persona.dni, "12345678")
 
+    def test_repr_persona(self):
+        persona = Persona("Juan", "Pérez", "12345678")
+        expected = "Persona: DNI: 12345678 Nombre: Juan Apellido: Pérez Ultima Idea: <no pensó en nada>"
+        self.assertEqual(str(persona), expected)
+
+    def test_pensar_incrementa_contador(self):
+        persona = Persona("Juan", "Pérez", "12345678")
+        persona.pensar("Hola mundo")
+        self.assertEqual(persona.pensamientos, 1)
+
+    def test_pensar_actualiza_ultima_idea(self):
+        persona = Persona("Juan", "Pérez", "12345678")
+        persona.pensar("Hola mundo")
+        self.assertEqual(persona.ultima_idea, "Hola mundo")
+
 if __name__ == "__main__":
     unittest.main()
